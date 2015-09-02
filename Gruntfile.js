653caa2f99c2fe7cacd84eddab9d2805b6289938
Tommi Ronkainen <tommi.j.ronkainen@microsoft.com>
2015-09-02 13:38:16 +0300

    Initial project files created.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },
+    jshint: {
+      src: {
+        src: ['src/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# chronicles_of_grunt
-Wigy's Chronicles of Grunt
+# Wigy's Chronicles of Grunt
+
+Wigy's collection of useful utilities for Grunt.
+
+## License
+
+Copyright (c) 2015 Tommi Ronkainen  
+Licensed under the GPL-2.0 license.
+
+## Release History
+
