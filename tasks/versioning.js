3d406b9903ce11f9fec0036c15290550b4c94b12
Tommi Ronkainen <tommi.j.ronkainen@microsoft.com>
2015-09-15 15:32:08 +0300

    Use build configuration for versioning.

--- tasks/versioning.js
+++ tasks/versioning.js
@@ -7,7 +7,7 @@
 module.exports = function(grunt) {
     grunt.registerTask('versioning', 'Query and mark the version to the source files.', function(version) {
 
-        var options = this.options({file: null});
+        var config = grunt.config.get('build') || {options: {}};
         var pkg = grunt.file.readJSON('package.json');
 
         if (arguments.length === 0) {
@@ -21,17 +21,31 @@ module.exports = function(grunt) {
             if (!version.match(/^\d+\.\d+\.\d+(-beta)?$/)) {
                 grunt.fail.fatal("Invalid version '" + version + "'.");
             }
+            if (!(config.options.src && config.options.src.config)) {
+                grunt.fail.fatal("Cannot find configured 'build.options.src.config' variable.");
+            }
+
+            // Update package.
             var debugMode = (version.substr(version.length-4) === 'beta');
             pkg.version = version;
             grunt.file.write('package.json', JSON.stringify(pkg, null, 2));
-            grunt.log.ok("Updated version", pkg.version, "to package.json.");
-            if (options.file) {
-                var settings = grunt.file.read(options.file);
-                settings = settings.replace(/^VERSION\s*=\s*'.*'/gm, "VERSION = '" + pkg.version + "'");
-                settings = settings.replace(/^DEBUG\s*=\s[^;]*/gm, "DEBUG = " + debugMode.toString());
-                grunt.file.write(options.file, settings);
-                grunt.log.ok("Updated version", pkg.version, "to", options.file);
-                grunt.log.ok("Set the debug mode to", debugMode, "in", options.file);
+            grunt.log.ok("Set version", pkg.version, "to package.json.");
+
+            // Update other files.
+            var files = config.options.src.config;
+            for (var i=0; i<files.length; i++) {
+                var file = files[i];
+                var newSettings, settings = grunt.file.read(file);
+                newSettings = settings.replace(/^VERSION\s*=\s*'.*'/gm, "VERSION = '" + pkg.version + "'");
+                if (newSettings !== settings) {
+                    grunt.log.ok("Updated version", pkg.version, "to", file);
+                    settings = newSettings;
+                }
+                newSettings = settings.replace(/^DEBUG\s*=\s[^;]*/gm, "DEBUG = " + debugMode.toString());
+                if (newSettings !== settings) {
+                    grunt.log.ok("Set the debug mode to", debugMode, "in", file);
+                }
+                grunt.file.write(file, newSettings);
             }
         }
     });
