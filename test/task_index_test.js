b3a36613b7240fb37a1f30a0280cad7cde55725a
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2016-10-12 16:31:43 +0300

    Fix test.

--- test/task_index_test.js
+++ test/task_index_test.js
@@ -16,7 +16,7 @@ module.exports = {
         test.expect(7);
         runner.run('index');
         var content = runner.read('index.html');
-        test.ok(content.indexOf('<script src="lib/angular.min.js"></script>') > 0, "has no lib/angular.min.js");
+        test.ok(content.indexOf('<script src="output/libs/angular.min.js"></script>') > 0, "has no output/libs/angular.min.js");
         test.ok(content.indexOf('<script src="src/config.js"></script>') > 0, "has no src/config.js");
         test.ok(content.indexOf('<script src="src/models.js"></script>') > 0, "has no src/models.js");
         test.ok(content.indexOf('<script src="src/data/data1.js"></script>') > 0, "has no src/data/data1.js");
