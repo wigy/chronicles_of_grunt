81500d6281c6ff3a779787f225d9ebcf128ecfd9
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2016-11-17 13:32:11 +0200

    Fix test.

--- test/task_info_test.js
+++ test/task_info_test.js
@@ -10,7 +10,7 @@ module.exports = {
         test.deepEqual(files['Index files'], ['index.html'], "has incorrect index file list");
         test.deepEqual(files['Configuration and global utilities'], ['src/config.js'], "has incorrect configuration file list");
         test.deepEqual(files['Model files'], ['src/models.js'], "has incorrect model file list");
-        test.deepEqual(files['Data files'], ['src/data/data1.js', 'src/data/subdata/data2.js'], "has incorrect data file list");
+        test.deepEqual(files['Source data files'], ['src/data/data1.js', 'src/data/subdata/data2.js'], "has incorrect data file list");
         test.deepEqual(files['CSS-files'], ['src/test.css'], "has incorrect CSS file list");
 
         test.done();
